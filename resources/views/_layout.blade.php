2a7f2a19887ddb30a5ca4c0db3f68e4550ff7872
Freddy Heppell <freddy@Freddys-MacBook-Air.local>
2015-08-18 22:29:16 +0100

    Installed fontawesome

--- resources/views/_layout.blade.php
+++ resources/views/_layout.blade.php
@@ -12,6 +12,7 @@
     <link href="{{ elixir('css/all.css') }}" rel="stylesheet">
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
     <link href="//cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.0/sweetalert.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
     @yield('custom_files')
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
