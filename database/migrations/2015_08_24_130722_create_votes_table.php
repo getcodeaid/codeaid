deb5df46e9d78cdda7962ea672b4539e1b76a956
freddinator <f.heppell@gmail.com>
2015-08-25 22:29:14 +0100

    Added vote migration and model

---
+++ database/migrations/2015_08_24_130722_create_votes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('user_id')->unsigned();
+            $table->integer('reply_id')->unsigned();
+
+            $table->char('type');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('votes');
+    }
+}
