71bbb9775ab623f7ef368fa8ec8fcb1b5b857ac4
freddinator <f.heppell@gmail.com>
2015-08-28 17:51:33 +0100

    Added replacement for reply buttons when user is not signed in

--- resources/views/snips/show.blade.php
+++ resources/views/snips/show.blade.php
@@ -15,7 +15,7 @@
                 <div class="row">
                     <img src="{{ $reply->owner->avatar() }}" alt="User Avatar" class="img-circle img-responsive avatar">
                 </div>
-                @if(user() != null)
+                @if(Auth::check())
                 <div class="row">
                     <h3 class="text-center" id="vote-{{ $reply->id }}"><span class="text-primary vote" data-id="{{ $reply->id }}" data-type="up"><i class="fa fa-angle-up @if($reply->userVote()->type == "+") text-success @endif"></i></span> <span class="score">{{ $reply->voteCount() }}</span> <span class="vote text-primary" data-id="{{ $reply->id }}" data-type="down"><i class="fa fa-angle-down @if($reply->userVote()->type == "-") text-danger @endif"></i></span></h3>
                 </div>
@@ -37,12 +37,18 @@
     @endforeach
     <div class="row">
         <div class="col-md-2"></div>
+        @if(Auth::check())
         <div class="col-md-4">
             <button class="btn btn-primary btn-block btn-lg" id="btn-comment"><i class="fa fa-commenting"></i> Comment</button>
         </div>
         <div class="col-md-4">
             <button class="btn btn-primary btn-block btn-lg" id="btn-modification"><i class="fa fa-pencil"></i> Modify</button>
         </div>
+        @else
+        <div class="col-md-8">
+            <h3 class="text-center">Want to join the discussion? <a href="{{ action('Auth\AuthController@getLogin') }}">Sign in to CodeAid</a></h3>
+        </div>
+        @endif
     </div>
     <br>
     <div class="row">
@@ -72,6 +78,7 @@
                 </div>
             </div>
         </div>
+
     </div>
 @stop
 
