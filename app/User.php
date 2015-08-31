1269c31adcb369eb2d86e2edb32aaa721b2a0464
freddinator <f.heppell@gmail.com>
2015-08-31 09:27:39 +0100

    Upgraded to Laravel 5.1.11

--- app/User.php
+++ app/User.php
@@ -8,10 +8,11 @@ use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Zizaco\Entrust\Traits\EntrustUserTrait;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract, SluggableInterface
+class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract, SluggableInterface
 {
     use Authenticatable, CanResetPassword, EntrustUserTrait, SluggableTrait;
 
